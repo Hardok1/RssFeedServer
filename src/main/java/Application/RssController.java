bee5d82a03a599cf504e1f2ac8c8ced38862a077
Hardok <hardokpl@gmal.com>
2017-12-17 11:39:58 +0100

    Dodano zabezpieczenie przed czasami pojawiajacym sie bledem (gdy description nie zawieral znacznika </a>)

--- src/main/java/Application/RssController.java
+++ src/main/java/Application/RssController.java
@@ -39,13 +39,22 @@ public class RssController {
 
                 //Wydzielanie opisu/ pozbywanie sie linkow/obrazkow
                 String line = entry.getDescription().getValue();
-                String description = line;     //Do przechowywania opisu przefiltrowanego
+                String description;    //Do przechowywania opisu przefiltrowanego
                 if (line.contains("</a>")) {
                     int firstPosition = line.indexOf("</a>");
                     description = line.substring(firstPosition);
                     description = description.replace("</a>", "");
                     int lastPosition = description.lastIndexOf("</p>");
                     description = description.substring(0, lastPosition);
+                } //W przypadku gdyby wpis nie zawieral znacznika "</a>"
+                else if (line.contains("<p>")) {
+                    int firstPosition = line.indexOf("<p>");
+                    description = line.substring(firstPosition);
+                    description = description.replace("<p>", "");
+                    int lastPosition = description.lastIndexOf("</p>");
+                    description = description.substring(0, lastPosition);
+                } else {
+                    description = "Nie powiodlo sie poprawne zaladowanie opisu";
                 }
 
                 //Po przefiltrowaniu mozemy bezpiecznie dodac obiekt do kolekcji
